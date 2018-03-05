fde73a861eb04f45a60cc71fbcb99e47b6b1fcd1
Chan Lee <chan.lee@nsuslab.com>
2018-03-05 17:23:05 +0900

    code for view on mobile

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
 
 const appRoutes: Routes = [
+    { path: '', component: HeaderComponent, outlet: 'header' },
+    { path: '', component: FooterComponent, outlet: 'footer' },
     { path: '', redirectTo: 'main', pathMatch: 'full' },
     { path: 'main', loadChildren: 'app/main/main.module#MainModule' }
 ];
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
 
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        HeaderComponent,
+        FooterComponent
     ],
     imports: [
         BrowserModule,
