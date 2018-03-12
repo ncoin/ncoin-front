f06b39a33b804c80ce20feb44a5d7912483ea614
Chan Lee <chan.lee@nsuslab.com>
2018-03-12 14:39:32 +0900

    add more effects

--- client/src/app/main/main.component.ts
+++ client/src/app/main/main.component.ts
@@ -1,15 +1,56 @@
-import { Component, OnInit } from '@angular/core';
+import {
+    Component, HostListener, OnInit, ElementRef, AfterViewChecked, AfterContentInit, AfterContentChecked,
+    AfterViewInit, ChangeDetectorRef
+} from '@angular/core';
+import { element } from 'protractor';
 
 @Component({
-  selector: 'app-main',
-  templateUrl: './main.component.html',
-  styleUrls: ['./main.component.scss']
+    selector: 'app-main',
+    templateUrl: './main.component.html',
+    styleUrls: ['./main.component.scss']
 })
-export class MainComponent implements OnInit {
+export class MainComponent implements AfterViewInit {
+    scrollTop: number;
+    windowHeight: number;
 
-  constructor() { }
+    @HostListener('window:scroll', [])
+    @HostListener('window:resize', [])
+    onWindowScroll() {
+        this.windowHeight = window.innerHeight;
+        this.scrollTop = window.pageYOffset;
+    }
 
-  ngOnInit() {
-  }
+    isViewing(el: HTMLElement) {
+        const h = el.offsetHeight;
+        const t = el.offsetTop;
+        const wh = this.windowHeight;
+        const wt = this.scrollTop;
+
+        return wt + wh * 0.8 > t && wt < t + h * 0.8;
+    }
+
+    elementBGY(el: HTMLElement) {
+        const h = el.offsetHeight;
+        const t = el.offsetTop;
+        const wh = this.windowHeight;
+        const wt = this.scrollTop;
+
+        let ret = h * (1 - (wh + wt - t) / h);
+        ret = wh + wt - t - h;
+        if (wt < t && ret > 0 ) {
+            ret = 0;
+        } else if (wt >= t && wh > h) {
+            ret = ret + h - wh;
+        }
+
+        return ret;
+    }
+
+    constructor(private elementRef: ElementRef, private cd: ChangeDetectorRef) {
+    }
+
+    ngAfterViewInit() {
+        this.cd.detectChanges();
+    }
 
 }
